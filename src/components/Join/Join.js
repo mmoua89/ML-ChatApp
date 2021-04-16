45dcaa46dc53713294d1c67394c7fa79cc34f652
Devin Suy <54761385+devinsuy@users.noreply.github.com>
2021-04-15 18:16:28 -0700

    Added press enter key to submit

--- src/components/Join/Join.js
+++ src/components/Join/Join.js
@@ -3,6 +3,13 @@ import { Link } from "react-router-dom";
 
 import './Join.css';
 
+// Submit if user presses enter key
+function checkKey(event){
+    if (event.key === 'Enter') {
+        document.getElementById("JoinButton").click();
+    }
+}
+
 export default function SignIn() {
   const [name, setName] = useState('');
 
@@ -11,10 +18,13 @@ export default function SignIn() {
       <div className="joinInnerContainer">
         <h1 className="heading">SA Chat</h1>
         <div>
-          <input placeholder="Enter your name" className="joinInput" type="text" onChange={(event) => setName(event.target.value)} />
+          <input 
+            id="NameBox" placeholder="Enter your name" className="joinInput" type="text" 
+            onChange={(event) => setName(event.target.value)} onKeyPress={(event) => checkKey(event)}
+          />
         </div>
         <Link onClick={e => (!name) ? e.preventDefault() : null} to={`/chat?name=${name}`}>
-          <button className={'button mt-20'} type="submit">Start Chatting</button>
+          <button id="JoinButton" className={'button mt-20'} type="submit">Start Chatting</button>
         </Link>
       </div>
     </div>
